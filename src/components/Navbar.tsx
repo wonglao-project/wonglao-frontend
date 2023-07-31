7792def456a97d0967bf0d456987488b841f0fc0
Sujaree Inthanarm <ppanng.s@pang-3.local>
2023-07-31 15:19:08 +0700

    add effect to homepage and navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,53 @@
-import { Link } from "react-router-dom"
-import { useAuth } from "../providers/AuthProvider"
-import { Map } from "lucide-react"
+import { Link } from "react-router-dom";
+import { useAuth } from "../providers/AuthProvider";
+import { Map } from "lucide-react";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-  const { isLoggedIn, logout } = useAuth()
+  const { isLoggedIn, logout } = useAuth();
 
   return (
-    <nav className='sticky bg-opacity-30 backdrop-blur-lg backdrop-filter'>
-      <div className='flex justify-between h-16 items-center px-12'>
-        <Link to={"/"}>
-          <p className='font-sans'>WONGLAO</p>
-        </Link>
-        <div className='flex gap-8'>
+    <nav className="sticky bg-opacity-30 backdrop-blur-lg backdrop-filter">
+      <div className="flex justify-between h-16 items-center px-12">
+        <motion.button whileHover={{ scale: 1.3, color: "#303234" }}>
+          <Link to={"/"}>
+            <p className="font-sans">WONGLAO</p>
+          </Link>
+        </motion.button>
+
+        <div className="flex gap-8">
           {isLoggedIn ? (
             <button onClick={logout}>Log out</button>
           ) : (
             <>
               <Link to={"/user/login"}>
-                <p>Login</p>
+                <motion.button
+                  whileHover={{
+                    scale: 1.3,
+                    color: "#303234",
+                  }}
+                >
+                  <p>Login</p>
+                </motion.button>
               </Link>
-              <Link to={"/user/register"}>
-                <p>Register</p>
-              </Link>
+
+              <motion.button whileHover={{ scale: 1.3, color: "#303234" }}>
+                <Link to={"/user/register"}>
+                  <p>Register</p>
+                </Link>
+              </motion.button>
             </>
           )}
-          <Link to={"/map"}>
-            <Map />
-          </Link>
+
+          <motion.button whileHover={{ scale: 1.3, color: "#303234" }}>
+            <Link to={"/map"}>
+              <Map />
+            </Link>
+          </motion.button>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom"
-import MapComp from "../components/MapComp"
+import { Link } from "react-router-dom";
+import MapComp from "../components/MapComp";
+import { motion } from "framer-motion";
 
 interface IMenu {
-  href: string
-  text: string
-  imgSrc: string
-  customClassname?: string
+  href: string;
+  text: string;
+  imgSrc: string;
+  customClassname?: string;
 }
 
 const menu: IMenu[] = [
@@ -27,52 +28,65 @@ const menu: IMenu[] = [
     imgSrc: "/img/product.png",
     customClassname: "left-[10%]",
   },
-]
+];
 
 const Home = () => {
   return (
     <div>
-      <header>
-        <img src='/img/banner.png' alt='banner' />
-      </header>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 1.5 }}
+      >
+        <header>
+          <img src="/img/banner.png" alt="banner" />
+        </header>
+      </motion.div>
+
       <main>
-        <div className='flex flex-row'>
-          <div className='m-20 w-[50%] h-[500px]'>
-            <MapComp />
-          </div>
-          <div className='m-20 w-[50%]'>
-            <h3>INFO</h3>
-            <p className='mt-5'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Laboriosam reprehenderit quisquam doloribus repellendus fugit
-              sapiente esse necessitatibus, nihil voluptate quod aspernatur quos
-              sint optio, consequuntur quia dignissimos aperiam nostrum
-              veritatis?
-            </p>
-          </div>
-        </div>
-        <div className='flex flex-row m-20 justify-between'>
-          {menu.map(({ href, imgSrc, text, customClassname }) => (
-            <div key={imgSrc} className='relative'>
-              <Link to={href}>
-                <img src={imgSrc} alt='bar' />
-                <p
-                  className={`absolute text-6xl font-extralight text-white top-[45%] ${
-                    customClassname ?? ""
-                  }`}
-                >
-                  {text}
-                </p>
-              </Link>
+        <motion.div>
+          <div className="flex flex-row sticky">
+            <div className="m-20 w-[50%] h-[500px]">
+              <MapComp />
             </div>
-          ))}
-        </div>
+            <div className="m-20 w-[50%]">
+              <h3>INFO</h3>
+              <p className="mt-5">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam reprehenderit quisquam doloribus repellendus fugit
+                sapiente esse necessitatibus, nihil voluptate quod aspernatur
+                quos sint optio, consequuntur quia dignissimos aperiam nostrum
+                veritatis?
+              </p>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div>
+          <div className="flex flex-row m-20 justify-between">
+            {menu.map(({ href, imgSrc, text, customClassname }) => (
+              <div key={imgSrc} className="relative">
+                <Link to={href}>
+                  <img src={imgSrc} alt="bar" />
+                  <p
+                    className={`absolute text-6xl font-extralight text-white top-[45%] ${
+                      customClassname ?? ""
+                    }`}
+                  >
+                    {text}
+                  </p>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </motion.div>
       </main>
+
       <footer>
         <p>FOOTER</p>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
