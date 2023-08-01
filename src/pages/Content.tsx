c1906d0f26ae89adbad62cf4ee0e795e1fc8e340
Sujaree Inthanarm <ppanng.s@pang-3.local>
2023-08-01 17:52:58 +0700

    edit effect

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,49 +1,49 @@
-import { useState } from "react"
-import ContentList from "../components/ContentList"
-import { EnumSellerCategory, SellerCategory } from "../types/types"
+import { useState } from "react";
+import ContentList from "../components/ContentList";
+import { EnumSellerCategory, SellerCategory } from "../types/types";
 
 const Content = () => {
   const [selectedCategory, setSelectedCategory] = useState<SellerCategory>(
     EnumSellerCategory.BREWER
-  )
+  );
 
   return (
     <>
-      <ul className='flex flex-row mx-20 border-b border-[#797979]/40'>
+      <ul className="flex flex-row mx-20 border-b border-[#797979]/40">
         <li
-          className='p-5 hover:cursor-pointer'
+          className="p-5 hover:cursor-pointer"
           onClick={() => setSelectedCategory(EnumSellerCategory.BAR)}
         >
           BAR
         </li>
         <li
-          className='p-5 hover:cursor-pointer'
+          className="p-5 hover:cursor-pointer"
           onClick={() => setSelectedCategory(EnumSellerCategory.BREWER)}
         >
           BREWER
         </li>
         <li
-          className='p-5 hover:cursor-pointer'
+          className="p-5 hover:cursor-pointer"
           onClick={() => setSelectedCategory(EnumSellerCategory.PRODUCT)}
         >
           PRODUCT
         </li>
       </ul>
-      <div className='flex flex-row mx-20 justify-between'>
-        <div className='flex flex-col'>
-          <p className='pb-3 px-20 pt-20 border-b border-[#797979]/40'>
+      <div className="flex flex-row mx-20 justify-between">
+        <div className="flex flex-col">
+          <p className="pb-3 px-20 pt-20 border-b border-[#797979]/40">
             Filter
           </p>
-          <ul className='py-5 px-20'>
+          <ul className="py-5 px-20">
             <li>Beer</li>
             <li>Gin</li>
             <li>Wine</li>
           </ul>
         </div>
-        <div className='grid grid-cols-3 gap-32 p-20'>
-          <img src='/img/list-product.png' alt='product list' />
-          <img src='/img/list-product.png' alt='product list' />
-          <img src='/img/list-product.png' alt='product list' />
+        <div className="grid grid-cols-3 gap-32 p-20">
+          <img src="/img/list-product.png" alt="product list" />
+          <img src="/img/list-product.png" alt="product list" />
+          <img src="/img/list-product.png" alt="product list" />
         </div>
       </div>
       {selectedCategory === EnumSellerCategory.PRODUCT ? (
@@ -53,7 +53,7 @@ const Content = () => {
         <ContentList selectedCategory={selectedCategory} />
       )}
     </>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
