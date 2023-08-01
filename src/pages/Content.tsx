31f811b6ebce89a8d93e769a5965ac0343beccb9
Sujaree Inthanarm <ppanng.s@pang-3.local>
2023-08-02 00:01:01 +0700

    fix hover effect

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,24 +1,24 @@
-import { useState } from "react"
-import ContentList from "../components/ContentList"
-import { EnumSellerCategory, SellerCategory } from "../types/types"
-import ProductList from "../components/ProductList"
-import { Link } from "react-router-dom"
-import { useAuth } from "../providers/AuthProvider"
-import { motion } from "framer-motion"
+import { useState } from "react";
+import ContentList from "../components/ContentList";
+import { EnumSellerCategory, SellerCategory } from "../types/types";
+import ProductList from "../components/ProductList";
+import { Link } from "react-router-dom";
+import { useAuth } from "../providers/AuthProvider";
+import { motion } from "framer-motion";
 
 const Content = () => {
-  const { isLoggedIn } = useAuth()
+  const { isLoggedIn } = useAuth();
   const [selectedCategory, setSelectedCategory] = useState<SellerCategory>(
     EnumSellerCategory.BREWER
-  )
+  );
 
   return (
     <>
-      <div className='flex flex-row mx-20 border-b border-[#797979]/40 justify-between'>
-        <ul className='flex flex-row'>
+      <div className="flex flex-row mx-20 border-b border-[#797979]/40 justify-between">
+        <ul className="flex flex-row">
           <motion.button whileHover={{ scale: 1.3, color: "#303234" }}>
             <li
-              className='p-5 hover:cursor-pointer'
+              className="p-5 hover:cursor-pointer hover:underline"
               onClick={() => setSelectedCategory(EnumSellerCategory.BREWER)}
             >
               BREWER
@@ -27,7 +27,7 @@ const Content = () => {
 
           <motion.button whileHover={{ scale: 1.3, color: "#303234" }}>
             <li
-              className='p-5 hover:cursor-pointer'
+              className="p-5 hover:cursor-pointer hover:underline"
               onClick={() => setSelectedCategory(EnumSellerCategory.BAR)}
             >
               BAR
@@ -36,18 +36,18 @@ const Content = () => {
 
           <motion.button whileHover={{ scale: 1.3, color: "#303234" }}>
             <li
-              className='p-5 hover:cursor-pointer'
+              className="p-5 hover:cursor-pointer hover:underline"
               onClick={() => setSelectedCategory(EnumSellerCategory.PRODUCT)}
             >
               PRODUCT
             </li>
           </motion.button>
         </ul>
-        <div className='flex items-center mx-5'>
+        <div className="flex items-center mx-5">
           {isLoggedIn && (
             <Link
               to={"/create"}
-              className='bg-gray-200  rounded-lg text-white hover:bg-[#797979] text-lg items-center p-2'
+              className="bg-gray-200  rounded-lg text-white hover:bg-[#797979] text-lg items-center p-2"
             >
               CREATE
             </Link>
@@ -55,18 +55,18 @@ const Content = () => {
         </div>
       </div>
 
-      <div className='flex flex-row mx-20 justify-between'>
-        <div className='flex flex-col'>
-          <p className='pb-3 px-20 pt-20 border-b border-[#797979]/40'>
+      <div className="flex flex-row mx-20 justify-between">
+        <div className="flex flex-col">
+          <p className="pb-3 px-20 pt-20 border-b border-[#797979]/40">
             Filter
           </p>
-          <ul className='py-5 px-20'>
+          <ul className="py-5 px-20">
             <li>Gin</li>
             <li>Rum</li>
             <li>White Spirit</li>
           </ul>
         </div>
-        <div className=''>
+        <div className="">
           {/* grid grid-cols-3 gap-32 p-20 */}
           {selectedCategory === EnumSellerCategory.PRODUCT ? (
             <ProductList />
@@ -76,7 +76,7 @@ const Content = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
