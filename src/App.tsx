4ff2e731a937ffda94f28b7d2186e95e66a85bd1
Sujaree Inthanarm <ppanng.s@pang-3.local>
2023-08-01 15:34:11 +0700

    remove page transition

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Create from "./pages/Create";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Content from "./pages/Content";
-import { AnimatePresence } from "framer-motion";
+
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -20,17 +20,18 @@ function App() {
   return (
     <>
       <Navbar />
-      <AnimatePresence mode="wait">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/content/:id" element={<Info />} />
-          <Route path="/content" element={<Content />} />
-          <Route path="/map" element={<Map />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/user/login" element={<Login />} />
-          <Route path="/user/register" element={<Register />} />
-        </Routes>
-      </AnimatePresence>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/content/:id" element={<Info />} />
+        <Route path="/content" element={<Content />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/create" element={<Create />} />
+
+        <Route path="/user/login" element={<Login />} />
+
+        <Route path="/user/register" element={<Register />} />
+      </Routes>
     </>
   );
 }
