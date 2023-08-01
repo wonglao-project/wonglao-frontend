b70646da02a8d3100f4d0af4e3a4ff5df8de59e1
Sujaree Inthanarm <ppanng.s@pang-3.local>
2023-08-01 23:26:20 +0700

    fix: remove framer motion from

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,37 @@
-import Navbar from "./components/Navbar"
-import { Route, Routes } from "react-router-dom"
-import { useEffect } from "react"
-import Home from "./pages/Home"
-import Info from "./pages/Info"
-import Map from "./pages/Map"
-import Create from "./pages/Create"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Content from "./pages/Content"
-import AOS from "aos"
-import "aos/dist/aos.css"
-import CreateProduct from "./pages/CreateProduct"
-import { AnimatePresence } from "framer-motion"
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import Home from "./pages/Home";
+import Info from "./pages/Info";
+import Map from "./pages/Map";
+import Create from "./pages/Create";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Content from "./pages/Content";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import CreateProduct from "./pages/CreateProduct";
 
 function App() {
   useEffect(() => {
-    AOS.init()
-  }, [])
+    AOS.init();
+  }, []);
 
   return (
     <>
       <Navbar />
-      <AnimatePresence mode='wait'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/content' element={<Content />} />
-          <Route path='/content/:id' element={<Info />} />
-          <Route
-            path='/content/:id/product/create'
-            element={<CreateProduct />}
-          />
-          <Route path='/map' element={<Map />} />
-          <Route path='/create' element={<Create />} />
-          <Route path='/user/login' element={<Login />} />
-          <Route path='/user/register' element={<Register />} />
-        </Routes>
-      </AnimatePresence>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/content" element={<Content />} />
+        <Route path="/content/:id" element={<Info />} />
+        <Route path="/content/:id/product/create" element={<CreateProduct />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/user/login" element={<Login />} />
+        <Route path="/user/register" element={<Register />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
